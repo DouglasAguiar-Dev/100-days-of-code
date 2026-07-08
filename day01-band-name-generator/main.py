959c7e4740ab3aa2fcc3964e23a5a35d48c9d806
DouglasAguiar-Dev <contato.douglas.silva.aguiar@gmail.com>
2026-07-08 19:13:33 +0100

    Improvement on code structure, code debugging, and input handling

--- day01-band-name-generator/main.py
+++ day01-band-name-generator/main.py
@@ -1,18 +1,32 @@
 print("Welcome to Band Name Generator by Douglas Aguiar")
 
-city_name = ""
-while city_name == "":
-    print("Oops! You forgot to type your city name.")
-    city_name = input("What is your favorite city name?\n").strip()
-city_title = city_name.title()
-
-pet_name = ""
-while pet_name == "":
-    print("Oops! You forgot to type your pet's name.")
-    pet_name = input("Now enter your pet's name:\n").strip()
-pet_title = pet_name.title()
-
-print("The name of your Band could be:",city_title + " " + pet_title)
-
-
-
+while True:
+    # Handle input city
+    while True:
+        city_name = input("What is your favorite city name?\n").strip().title()
+        if city_name == "":
+            print("Oops! You forgot to type your city name.")
+            continue 
+        break
+            
+    # Handle input pet   
+    while True: 
+        pet_name = input("Now enter your pet's name:\n").strip().title()
+        if pet_name == "":
+            print("Oops! You forgot to type your pet's name.")
+            continue 
+        break
+            
+    # print names
+    print(f"The name of your Band could be: {city_name} {pet_name}")
+        
+    # Restart or exit the program 
+    while True:
+        should_continue = input("Type 'r' to retry a new name or 'n' to exit the program:\n").strip().lower()
+        if should_continue == 'n':
+            print("bye!")
+            exit()  # Using exit() here to completely stop the program 
+        elif should_continue == 'r':
+            break # Escapes this mini-loop, allowing the master loop to restart
+        else: 
+            print("Invalid input, Try again.")
